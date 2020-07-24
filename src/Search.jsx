b103068d399447dc3e1fd7d1d3b0636a25e40d86
Kwakcena <rhkrgudwh@gmail.com>
2020-07-24 15:58:16 +0900

    고양이 종 검색에 대한 change, click 이벤트 테스트
    고양이 종을 검색하면서 입력창에 입력을 할 때 change 이벤트가 발생하는지
    테스트 한다.
    
    search 버튼을 누르면 onClick 이벤트가 발생하는지 테스트 한다.

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 
-export default function Search() {
+export default function Search({ value, onChange, onSubmit }) {
   return (
     <div>
       <label htmlFor="input-breeds">고양이 검색 </label>
       <input
         id="input-breeds"
+        name="breeds"
+        value={value}
         type="text"
         placeholder="고양이 종을 검색하세요"
+        onChange={onChange}
       />
       <button
         type="button"
+        onClick={onSubmit}
       >
         search
       </button>
--- src/Search.test.jsx
+++ src/Search.test.jsx
@@ -1,25 +1,60 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import Search from './Search';
 
 describe('Search', () => {
-  function renderSearch() {
+  const handleSubmit = jest.fn();
+  const handleChange = jest.fn();
+
+  beforeEach(() => {
+    handleChange.mockClear();
+    handleSubmit.mockClear();
+  });
+
+  function renderSearch({ value } = {}) {
     return render((
-      <Search />
+      <Search
+        value={value}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
     ));
   }
 
   it('renders input control', () => {
-    const { getByLabelText } = renderSearch();
+    const value = 'bengal';
+    const { getByLabelText } = renderSearch({ value });
 
-    expect(getByLabelText('고양이 검색')).toBeInTheDocument();
+    const input = getByLabelText('고양이 검색');
+    expect(input.value).toBe(value);
   });
 
   it('renders "search" button', () => {
     const { getByText } = renderSearch();
 
     expect(getByText('search')).toBeInTheDocument();
+  });
+
+  it('listens change event', () => {
+    const { getByLabelText } = renderSearch();
+
+    const input = getByLabelText('고양이 검색');
+    fireEvent.change(input, {
+      target: {
+        value: 'bengal',
+      },
+    });
+
+    expect(handleChange).toBeCalled();
+  });
+
+  it('listens click event', () => {
+    const { getByText } = renderSearch();
+
+    fireEvent.click(getByText('search'));
+
+    expect(handleSubmit).toBeCalled();
   });
 });
